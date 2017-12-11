45811897cc9d4902eb48b9a066559dfb289ab8a0
Joshua Weinstein <jweinstein@splunk.com>
2017-12-11 13:10:28 -0800

    object work

--- include/WindObject.h
+++ include/WindObject.h
@@ -2,4 +2,24 @@
 #define WIND_OBJECT_H
 // contains windobject definition
 
+
+typedef enum
+{
+        WindType_None,
+        WindType_Int,
+        WindType_Add,
+        WindType_Sub
+} WindType;
+
+typedef union
+{
+        int _int;
+} WindValue;
+
+typedef struct
+{
+        WindValue value;
+        WindType type;
+} WindObject;
+
 #endif
---
+++ src/resources/Main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "WindObject.h"
+
+int main(int argc, char const *argv[]) {
+        puts("foo");
+        return 0;
+}
