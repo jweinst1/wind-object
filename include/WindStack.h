f3af723339fab37cb340e08afd68acca247db8d1
Joshua Weinstein <jweinstein@splunk.com>
2017-12-11 14:56:37 -0800

    evaluating stack

--- include/WindStack.h
+++ include/WindStack.h
@@ -2,12 +2,40 @@
 #define WIND_STACk_H
 
 #include "WindObject.h"
+#include "Instruction.h"
+#include <stdlib.h>
+
+
+#define WindStack_INIT(name, length) \
+        WindStack name; \
+        name.begin = malloc(sizeof(StackNode) * length); \
+        name.mark = begin; \
+        name.end = name.begin + length;
 
 typedef struct
 {
-        WindObject* begin;
-        WindObject* mark;
-        WindObject* end;
+        WindObject* obj;
+        Instruction ins;
+} StackNode;
+
+/** NodeState
+ * - Keeps track of if the node still needs to evaluate, or is done and ready to
+ * move back or finish entire program process
+ *
+ */
+typedef enum
+{
+        NodeState_Running,
+        NodeState_Done
+}NodeState;
+
+
+typedef struct
+{
+        StackNode* begin;
+        StackNode* mark;
+        StackNode* end;
+        NodeState state;
 } WindStack;
 
 #endif
--- src/resources/Main.c
+++ src/resources/Main.c
@@ -3,9 +3,6 @@
 #include "WindObject.h"
 
 int main(int argc, char const *argv[]) {
-        WindObject* stack = malloc(sizeof(WindObject) * 10);
-        stack[0].type = WindType_Add;
-        stack[1].type = WindType_ExpStart;
 
         return 0;
 }
